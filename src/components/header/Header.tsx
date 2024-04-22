1abb342d49fa94fc4dfaecfac122fbeea8e3022c
John Correa <jecl2912@gmail.com>
2024-04-22 14:51:40 -0500

    fix: api fetch

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,17 +2,22 @@
 import Image from "next/image";
 import logo from '../../../public/plaza-soft.png'
 import { useStoreData } from '@/hooks/data'
+import {cn} from "@/lib/utils";
 
 const Header = () => {
 
     const dataDummy = useStoreData()
 
     return (
-        <header className="flex justify-center items-center py-4 shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] sticky top-0 bg-white z-[999]">
+        <header
+            className={cn(`flex justify-center items-center py-4 
+            shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] sticky top-0 
+            ${dataDummy?.headerColorHEX ? `bg-[#${dataDummy?.headerColorHEX}]` : 'bg-white'} z-[999]`)}>
             {
                 dataDummy ? (
-                    <Image src={dataDummy?.logo} alt={dataDummy?.portalTittle} width={80} height={80} className={'object-cover'}/>
-                ): (
+                    <Image src={dataDummy?.logo} alt={dataDummy?.portalTittle} width={80} height={80}
+                           className={'object-cover'}/>
+                ) : (
                     <Image src={logo} alt="logo"/>
                 )
             }
--- src/hooks/data.tsx
+++ src/hooks/data.tsx
@@ -1,20 +1,39 @@
 'use client'
 import { useEffect, useState } from 'react';
-import { useSearchParams } from 'next/navigation'
-import dataDummy from '@/components/data/data';
+import { useSearchParams } from 'next/navigation';
 
 export function useStoreData(): any | null {
-    const searchParams = useSearchParams()
+    const searchParams = useSearchParams();
 
     const [storeData, setStoreData] = useState<any>(null);
 
     useEffect(() => {
-        const storeName = searchParams.get('storeName');
-        if (typeof storeName === 'string') {
-            const filteredData = dataDummy.find((item: any) => item.portalTittle === storeName);
-            setStoreData(filteredData);
-        }
+        const fetchData = async () => {
+            const storeName = searchParams.get('storeName');
+            if (typeof storeName === 'string') {
+                try {
+                    const response = await fetch(`https://devapi.plazasoftware.com.co:8100/api/CustomerPortal/Configuration?StoreName=${encodeURIComponent(storeName)}`);
+                    if (!response.ok) {
+                        throw new Error('Failed to fetch data');
+                    }
+
+                    const contentType = response.headers.get('content-type');
+                    if (!contentType || !contentType.includes('application/json')) {
+                        throw new Error('Response is not in JSON format');
+                    }
+
+                    const data = await response.json();
+                    setStoreData(data);
+                } catch (error) {
+                    console.error('Error fetching store data:', error);
+                }
+            }
+        };
+
+        fetchData();
     }, [searchParams]);
+
+    console.log('storeData', storeData);
 
     return storeData;
 }
